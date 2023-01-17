9943517746f116553e3f63eed0591699449d382c
Philippe Altherr <philippe.altherr@gmail.com>
2023-01-17 16:33:36 +0100

    Change indent size to 2 (from 4) for shell scripts.

--- tests/test-support.bash
+++ tests/test-support.bash
@@ -6,145 +6,144 @@ set -eu
 # Test setup
 
 function setup_file() {
-    export MESSAGE="a-simple-test-message";
-    export UNKNOWN_COMMAND="unknown-command";
-    export TEST_FILE=tests/test-runner.zsh;
-    export CONTEXTS=$(grep -o 'ctx[_0-9A-Za-z]\+' $TEST_FILE);
+  export MESSAGE="a-simple-test-message";
+  export UNKNOWN_COMMAND="unknown-command";
+  export TEST_FILE=tests/test-runner.zsh;
+  export CONTEXTS=$(grep -o 'ctx[_0-9A-Za-z]\+' $TEST_FILE);
 
-    export TRACE_top=$($TEST_FILE eval echo '$funcfiletrace[1]');
-    local f;
-    for f in tic tac toe $CONTEXTS; do
-        export TRACE_$f=$($TEST_FILE eval "exec 3>&1; $f eval echo '\$funcfiletrace[1]' '1>&3'");
-    done;
+  export TRACE_top=$($TEST_FILE eval echo '$funcfiletrace[1]');
+  local f;
+  for f in tic tac toe $CONTEXTS; do
+    export TRACE_$f=$($TEST_FILE eval "exec 3>&1; $f eval echo '\$funcfiletrace[1]' '1>&3'");
+  done;
 }
 
 function setup() {
-    load '/usr/local/lib/bats-support/load.bash';
-    load '/usr/local/lib/bats-assert/load.bash';
-    callers=(tic tac toe);
+  load '/usr/local/lib/bats-support/load.bash';
+  load '/usr/local/lib/bats-assert/load.bash';
+  callers=(tic tac toe);
 }
 
 ################################################################################
 # Helper functions
 
 function context_starts_subshell() {
-    local context=$1; shift 1;
-    grep -q "$context.*@subshell" $TEST_FILE;
+  local context=$1; shift 1;
+  grep -q "$context.*@subshell" $TEST_FILE;
 }
 
 function context_status_is_ignored() {
-    local context=$1; shift 1;
-    grep -q "$context.*@ignored" $TEST_FILE;
+  local context=$1; shift 1;
+  grep -q "$context.*@ignored" $TEST_FILE;
 }
 
 function context_command_is_condition() {
-    local context=$1; shift 1;
-    grep -q "$context.*@condition" $TEST_FILE;
+  local context=$1; shift 1;
+  grep -q "$context.*@condition" $TEST_FILE;
 }
 
 function enter-trace() {
-    function rec() {
-        local lvl=$1; shift 1;
-        local fun=$1; shift 1;
-        printf "Lvl%02i Enter %s\n" $lvl $fun;
-        if [[ $# -gt 0 ]]; then
-            if [[ $fun = ctx_eval ]]; then
-                rec $(($lvl + 2)) "$@";
-            else
-                rec $(($lvl + 1)) "$@";
-            fi;
-        fi;
-    }
+  function rec() {
+    local lvl=$1; shift 1;
+    local fun=$1; shift 1;
+    printf "Lvl%02i Enter %s\n" $lvl $fun;
     if [[ $# -gt 0 ]]; then
-        rec 1 "$@";
+      if [[ $fun = ctx_eval ]]; then
+        rec $(($lvl + 2)) "$@";
+      else
+        rec $(($lvl + 1)) "$@";
+      fi;
     fi;
+  }
+  if [[ $# -gt 0 ]]; then
+    rec 1 "$@";
+  fi;
 }
 
 function leave-trace() {
-    function rec() {
-        local lvl=$1; shift 1;
-        local fun=$1; shift 1;
-        if [[ $# -gt 0 ]]; then
-            if [[ $fun = ctx_eval ]]; then
-                rec $(($lvl + 2)) "$@";
-            else
-                rec $(($lvl + 1)) "$@";
-            fi;
-        fi;
-        printf "Lvl%02i Leave %s\n" $lvl $fun;
-    }
+  function rec() {
+    local lvl=$1; shift 1;
+    local fun=$1; shift 1;
     if [[ $# -gt 0 ]]; then
-        rec 1 "$@";
+      if [[ $fun = ctx_eval ]]; then
+        rec $(($lvl + 2)) "$@";
+      else
+        rec $(($lvl + 1)) "$@";
+      fi;
     fi;
+    printf "Lvl%02i Leave %s\n" $lvl $fun;
+  }
+  if [[ $# -gt 0 ]]; then
+    rec 1 "$@";
+  fi;
 }
 
 function stack-trace() {
-    function rec() {
-        local ctx=$1; shift 1;
-        local fun=$1; shift 1;
-        [[ $# -gt 0 ]] && rec "$fun" "$@";
-        eval echo "\"at \$TRACE_$ctx($fun)\"";
-        # The builtin "eval" adds an extra stack frame.
-        if [[ $ctx = ctx_eval ]]; then
-            eval echo "\"at \$TRACE_$ctx((eval))\"";
-        fi;
-    }
-    if [[ $# -gt 0 ]]; then
-        rec "top" "$@";
+  function rec() {
+    local ctx=$1; shift 1;
+    local fun=$1; shift 1;
+    [[ $# -gt 0 ]] && rec "$fun" "$@";
+    eval echo "\"at \$TRACE_$ctx($fun)\"";
+    # The builtin "eval" adds an extra stack frame.
+    if [[ $ctx = ctx_eval ]]; then
+      eval echo "\"at \$TRACE_$ctx((eval))\"";
     fi;
+  }
+  if [[ $# -gt 0 ]]; then
+    rec "top" "$@";
+  fi;
 }
 
 function check() {
-    local command=( "${callers[@]}" "$@" );
-    echo "# Testing: $TEST_FILE ${command[@]}";
+  local command=( "${callers[@]}" "$@" );
+  echo "# Testing: $TEST_FILE ${command[@]}";
 
-    if [ -z ${expected_failure+x} ]; then
-        local expected_failure=1;
-    fi;
+  if [ -z ${expected_failure+x} ]; then
+    local expected_failure=1;
+  fi;
 
-    if [ -z ${expected_enter_trace+x} ]; then
-        local expected_enter_trace=$(enter-trace ${callers[@]});
-    fi;
+  if [ -z ${expected_enter_trace+x} ]; then
+    local expected_enter_trace=$(enter-trace ${callers[@]});
+  fi;
 
-    if [ -z ${expected_leave_trace+x} ]; then
-     if [ $expected_failure -eq 0 ]; then
-         local expected_leave_trace=$(leave-trace ${callers[@]});
-     else
-         local expected_leave_trace="";
-     fi;
-    fi;
-
-    if [ -z ${expected_stack_trace+x} ]; then
-        case $BATS_TEST_NAME in
-            test_abort-* ) local expected_stack_trace=$(stack-trace ${callers[@]} abort);;
-            test_usage-* ) local expected_stack_trace=$(stack-trace ${callers[@]});;
-            test_error-* ) local expected_stack_trace=$(stack-trace ${callers[@]} abort);;
-            *            ) echo "Unrecognised test: $BATS_TEST_NAME" >&2; exit 1;;
-        esac
-    fi;
-
-    run --separate-stderr $TEST_FILE "${command[@]}";
+  if [ -z ${expected_leave_trace+x} ]; then
     if [ $expected_failure -eq 0 ]; then
-        assert_success;
+      local expected_leave_trace=$(leave-trace ${callers[@]});
     else
-        assert_failure $expected_failure;
+      local expected_leave_trace="";
     fi;
-    assert_output "";
+  fi;
 
-    run $TEST_FILE "${command[@]}";
-    if [ $expected_failure -eq 0 ]; then
-        assert_success;
-    else
-        assert_failure $expected_failure;
-    fi;
-    assert_output - <<< $(
-        {
-            [ -z "$expected_enter_trace" ] || echo "$expected_enter_trace";
-            [ -z "${expected_message+x}" ] || echo "$expected_message";
-            [ -z "$expected_stack_trace" ] || echo "$expected_stack_trace";
-            [ -z "$expected_leave_trace" ] || echo "$expected_leave_trace";
-        });
+  if [ -z ${expected_stack_trace+x} ]; then
+    case $BATS_TEST_NAME in
+      test_abort-* ) local expected_stack_trace=$(stack-trace ${callers[@]} abort);;
+      test_usage-* ) local expected_stack_trace=$(stack-trace ${callers[@]});;
+      test_error-* ) local expected_stack_trace=$(stack-trace ${callers[@]} abort);;
+      *            ) echo "Unrecognised test: $BATS_TEST_NAME" >&2; exit 1;;
+    esac
+  fi;
 
+  run --separate-stderr $TEST_FILE "${command[@]}";
+  if [ $expected_failure -eq 0 ]; then
+    assert_success;
+  else
+    assert_failure $expected_failure;
+  fi;
+  assert_output "";
+
+  run $TEST_FILE "${command[@]}";
+  if [ $expected_failure -eq 0 ]; then
+    assert_success;
+  else
+    assert_failure $expected_failure;
+  fi;
+  assert_output - <<< $(
+    {
+      [ -z "$expected_enter_trace" ] || echo "$expected_enter_trace";
+      [ -z "${expected_message+x}" ] || echo "$expected_message";
+      [ -z "$expected_stack_trace" ] || echo "$expected_stack_trace";
+      [ -z "$expected_leave_trace" ] || echo "$expected_leave_trace";
+    });
 }
 
 ################################################################################
--- tests/usage.bats
+++ tests/usage.bats
@@ -6,82 +6,82 @@
 # Function "usage" tests
 
 function check-usage() {
-    check usage "$@";
+  check usage "$@";
 }
 
 @test "usage: Single argument" {
-    expected_message="toe: $MESSAGE";
-    check-usage $MESSAGE;
+  expected_message="toe: $MESSAGE";
+  check-usage $MESSAGE;
 }
 
 @test "usage: Top-level call" {
-    callers=();
-    expected_message="$TEST_FILE: $MESSAGE";
-    check-usage $MESSAGE;
+  callers=();
+  expected_message="$TEST_FILE: $MESSAGE";
+  check-usage $MESSAGE;
 }
 
 @test "usage: Explicit misused command" {
-    expected_message="toe: $MESSAGE";
-    expected_stack_trace=$(stack-trace tic tac toe);
-    check-usage -0 $MESSAGE;
+  expected_message="toe: $MESSAGE";
+  expected_stack_trace=$(stack-trace tic tac toe);
+  check-usage -0 $MESSAGE;
 
-    expected_message="tac: $MESSAGE";
-    expected_stack_trace=$(stack-trace tic tac);
-    check-usage -1 $MESSAGE;
+  expected_message="tac: $MESSAGE";
+  expected_stack_trace=$(stack-trace tic tac);
+  check-usage -1 $MESSAGE;
 
-    expected_message="tic: $MESSAGE";
-    expected_stack_trace=$(stack-trace tic);
-    check-usage -2 $MESSAGE;
+  expected_message="tic: $MESSAGE";
+  expected_stack_trace=$(stack-trace tic);
+  check-usage -2 $MESSAGE;
 
-    expected_message="$TEST_FILE: $MESSAGE";
-    expected_stack_trace=$(stack-trace);
-    check-usage -3 $MESSAGE;
+  expected_message="$TEST_FILE: $MESSAGE";
+  expected_stack_trace=$(stack-trace);
+  check-usage -3 $MESSAGE;
 
-    expected_message="usage: Valid command indexes range from 0 to 3, found \"4\".";
-    expected_stack_trace=$(stack-trace tic tac toe usage);
-    check-usage -4 $MESSAGE;
+  expected_message="usage: Valid command indexes range from 0 to 3, found \"4\".";
+  expected_stack_trace=$(stack-trace tic tac toe usage);
+  check-usage -4 $MESSAGE;
 
-    expected_message="usage: Valid command indexes range from 0 to 3, found \"42\".";
-    expected_stack_trace=$(stack-trace tic tac toe usage);
-    check-usage -42 $MESSAGE;
+  expected_message="usage: Valid command indexes range from 0 to 3, found \"42\".";
+  expected_stack_trace=$(stack-trace tic tac toe usage);
+  check-usage -42 $MESSAGE;
 }
 
 @test "usage: Miscellaneous argument combinations" {
-    expected_message="toe: message   with   spaces";
-    check-usage "message   with   spaces";
+  expected_message="toe: message   with   spaces";
+  check-usage "message   with   spaces";
 
-    expected_message="toe: --message starting with a dash";
-    check-usage -- "--message starting with a dash";
+  expected_message="toe: --message starting with a dash";
+  check-usage -- "--message starting with a dash";
 
-    expected_message="tic: --message starting with a dash";
-    expected_stack_trace=$(stack-trace tic);
-    check-usage -2 -- "--message starting with a dash";
+  expected_message="tic: --message starting with a dash";
+  expected_stack_trace=$(stack-trace tic);
+  check-usage -2 -- "--message starting with a dash";
 }
 
 @test "usage: Invalid options" {
-    expected_stack_trace=$(stack-trace tic tac toe usage);
+  expected_stack_trace=$(stack-trace tic tac toe usage);
 
-    expected_message="usage: Unrecognised option: \"-x\"";
-    check-usage -x $MESSAGE;
+  expected_message="usage: Unrecognised option: \"-x\"";
+  check-usage -x $MESSAGE;
 
-    expected_message="usage: Unrecognised option: \"--invalid-option\"";
-    check-usage --invalid-option $MESSAGE;
+  expected_message="usage: Unrecognised option: \"--invalid-option\"";
+  check-usage --invalid-option $MESSAGE;
 }
 
 @test "usage: Invalid messages" {
-    expected_stack_trace=$(stack-trace tic tac toe usage);
+  expected_stack_trace=$(stack-trace tic tac toe usage);
 
-    expected_message="usage: An error message is required.";
-    check-usage;
+  expected_message="usage: An error message is required.";
+  check-usage;
 
-    expected_message="usage: Multiple error messages aren't allowed, found: \"foo\" \"bar\".";
-    check-usage foo bar;
-    expected_message="usage: Multiple error messages aren't allowed, found: \"\" \"\".";
-    check-usage "" "";
+  expected_message="usage: Multiple error messages aren't allowed, found: \"foo\" \"bar\".";
+  check-usage foo bar;
+  expected_message="usage: Multiple error messages aren't allowed, found: \"\" \"\".";
+  check-usage "" "";
 
-    expected_message="usage: The error message may not be empty.";
-    check-usage "";
-    check-usage "$(printf " \t\n ")";
+  expected_message="usage: The error message may not be empty.";
+  check-usage "";
+  check-usage "$(printf " \t\n ")";
 }
 
 ################################################################################
